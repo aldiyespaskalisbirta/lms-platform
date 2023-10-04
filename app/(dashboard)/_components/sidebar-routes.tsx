b8f478e26fdffc571b5302a1a42f6c3c0e329c7c
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-10-05 03:08:55 +0700

    create dynamic-layout

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -2,7 +2,9 @@
 
 import SidebarItem from "./sidebar-item";
 import { LuLayoutDashboard } from "react-icons/lu";
-import { AiOutlineCompass } from "react-icons/ai";
+import { AiOutlineCompass, AiOutlineBars } from "react-icons/ai";
+import { BiBarChart } from "react-icons/bi";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
   {
@@ -17,8 +19,24 @@ const guestRoutes = [
   },
 ];
 
+const teacherRoutes = [
+  {
+    icon: AiOutlineBars,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BiBarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
 function SidebarRoutes() {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+
   return (
     <div className="flex flex-col w-full">
       {routes.map((route) => (
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,10 +1,35 @@
+"use client";
+
 import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { BiLogOut } from "react-icons/bi";
+import Link from "next/link";
 
 function NavbarRoutes() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button className="gap-1">
+            <BiLogOut />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
