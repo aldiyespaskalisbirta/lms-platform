3d60f0c935e5497f072804444153cc6120262eea
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-10-07 02:26:58 +0700

    create serach query, course-card course-list, hooks and any other'

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
-function SearchPage() {
-  return <div>SearchPage</div>;
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+
+import { getCourses } from "@/actions/get-courses";
+import SearchInput from "@/components/search-input";
+import Categories from "./_components/categories";
+import CoursesList from "@/components/courses-list";
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+async function SearchPage({ searchParams }: SearchPageProps) {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
+
+  return (
+    <>
+      <div className="px-6 pt-6 block md:hidden md:mb-0">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+        <CoursesList items={courses} />
+      </div>
+    </>
+  );
 }
 
 export default SearchPage;
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,36 +1,46 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { BiLogOut } from "react-icons/bi";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+
+import { BiLogOut } from "react-icons/bi";
+
+import { Button } from "@/components/ui/button";
+import SearchInput from "./search-input";
 
 function NavbarRoutes() {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button className="gap-1">
-            <BiLogOut />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Teacher mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton afterSignOutUrl="/" />
-    </div>
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button className="gap-1">
+              <BiLogOut />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher mode
+            </Button>
+          </Link>
+        )}
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </>
   );
 }
 
